668125a50f15a75fd4696d66c33d8f76332ca536
Mateusz Wilczyński <majko7@interia.pl>
2016-05-23 14:04:36 +0200

    Improve Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  get 'fajna_strona', to: 'home#index'
   get 'about', to: 'home#about'
   get 'terms', to: 'home#terms'
 end
