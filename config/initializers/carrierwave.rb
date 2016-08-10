259c296a6cba9ee817925f81f25b0ab9b61438f8
Mateusz Wilczyński <majko7@interia.pl>
2016-08-10 11:40:13 +0200

    Use S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     Settings.aws.key_id,
+    aws_secret_access_key: Settings.aws.secret_key,
+    region:                Settings.aws.region
+  }
+  config.fog_directory  = Settings.aws.bucket_name
+end
